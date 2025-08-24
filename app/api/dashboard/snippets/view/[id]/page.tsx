3211eca095b8637f375acf48d48472cf3944c3e1
harsh <dhankhar14804@gmail.com>
2025-08-24 09:38:43 +0530

    removed unnecessary usParams hook

--- app/api/dashboard/snippets/view/[id]/page.tsx
+++ app/api/dashboard/snippets/view/[id]/page.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import { useParams } from 'next/navigation'
 
 const page = async () => {
-    const { id } = useParams()
-    console.log(id)
     return (
         <div>page</div>
     )
